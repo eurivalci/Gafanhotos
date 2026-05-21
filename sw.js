8b87c92dda36ac0bfb05c750d10475c341c85225
eurivalci <eurivalci@gmail.com>
2026-05-20 21:57:51 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE = 'trekcalc-v1';
+var ASSETS = ['./index.html', './manifest.json', './sw.js'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      var network = fetch(e.request).then(function(res){
+        if(res && res.ok && e.request.method === 'GET'){
+          var clone = res.clone();
+          caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+        }
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || network;
+    })
+  );
+});
